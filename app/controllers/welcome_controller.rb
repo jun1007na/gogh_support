16c4c129cf3abb68386d02471a41bf7de3ec38db
Juna <dev.jj@icloud.com>
2018-04-20 21:44:10 +0900

    Implementation divided by day of the week

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,11 @@
 class WelcomeController < ApplicationController
   def home
-    @subjects = Subject.all
+    @mon_subjects = Subject.where(week: 1).order('time')
+    @tue_subjects = Subject.where(week: 2).order('time')
+    @wed_subjects = Subject.where(week: 3).order('time')
+    @thu_subjects = Subject.where(week: 4).order('time')
+    @fri_subjects = Subject.where(week: 5).order('time')
+    
   end
 
   def about
